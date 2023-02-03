4dafa2c0a11e1da637ccb8cdad08e9b85fc98725
Daria <daria.citak1703@gmail.com>
2023-02-03 13:41:04 +0100

    Update

--- admin_panel/src/components/chart/Chart.jsx
+++ admin_panel/src/components/chart/Chart.jsx
@@ -1,64 +1,100 @@
 import React from 'react'
 import "./chart.css" 
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
-
+import { useEffect, useMemo, useState } from "react";
+import { userRequest } from "../../requestMethods";
 
 export default function Chart() {
-    const data = [
-        {
-          name: 'Jan',
-          "Sales": 4000,
-        },
-        {
-          name: 'Feb',
-          "Sales": 2000,
-        },
-        {
-          name: 'Mar',
-          "Sales": 2000,
-        },
-        {
-          name: 'Apr',
-          "Sales": 2780,
-        },
-        {
-          name: 'May',
-          "Sales": 1890,
-        },
-        {
-          name: 'Jun',
-          "Sales": 2390,
-        },
-        {
-          name: 'Jul',
-          "Sales": 3490,
-        },
-        {
-          name: 'Aug',
-          "Sales": 1490,
-        },
-        {
-          name: 'Sep',
-          "Sales": 5090,
-        },
-        {
-          name: 'Oct',
-          "Sales": 3130,
-        },
-        {
-          name: 'Nov',
-          "Sales": 1130,
-        },
-        {
-          name: 'Dec',
-          "Sales": 6130,
-        },
-      ];
+    // const data = [
+    //     {
+    //       name: 'Jan',
+    //       "Sales": 4000,
+    //     },
+    //     {
+    //       name: 'Feb',
+    //       "Sales": 2000,
+    //     },
+    //     {
+    //       name: 'Mar',
+    //       "Sales": 2000,
+    //     },
+    //     {
+    //       name: 'Apr',
+    //       "Sales": 2780,
+    //     },
+    //     {
+    //       name: 'May',
+    //       "Sales": 1890,
+    //     },
+    //     {
+    //       name: 'Jun',
+    //       "Sales": 2390,
+    //     },
+    //     {
+    //       name: 'Jul',
+    //       "Sales": 3490,
+    //     },
+    //     {
+    //       name: 'Aug',
+    //       "Sales": 1490,
+    //     },
+    //     {
+    //       name: 'Sep',
+    //       "Sales": 5090,
+    //     },
+    //     {
+    //       name: 'Oct',
+    //       "Sales": 3130,
+    //     },
+    //     {
+    //       name: 'Nov',
+    //       "Sales": 1130,
+    //     },
+    //     {
+    //       name: 'Dec',
+    //       "Sales": 6130,
+    //     },
+    //   ];
+
+    const [orderStats, setOrderStats] = useState([]);
+
+  const MONTHS = useMemo(
+    () => [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Agu",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ],
+    []
+  );
+
+  useEffect(() => {
+    const getStats = async () => {
+      try {
+        const res = await userRequest.get("/orders/income");
+        res.data.map((item) =>
+          setOrderStats((prev) => [
+            ...prev,
+            { name: MONTHS[item._id - 1], "Sales": item.total },
+          ])
+        );
+      } catch {}
+    };
+    getStats();
+  }, [MONTHS]);
   return (
     <div className='chartContainer'>
         <h3 className='chartTitle'>Income Analytics</h3>
         <ResponsiveContainer className="diagram"  aspect={4/1}>
-            <LineChart data={data}>
+            <LineChart data={orderStats}>
                 <XAxis dataKey="name" width="100%" stroke='#808080' />
                 <Line type="monotone" dataKey="Sales"/>
                 <Tooltip/>
--- admin_panel/src/components/featureInfo/FeatureInfo.jsx
+++ admin_panel/src/components/featureInfo/FeatureInfo.jsx
@@ -2,15 +2,31 @@ import React from 'react'
 import "./featureInfo.css" 
 import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
 import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
+import { useEffect, useState } from "react";
+import { userRequest } from "../../requestMethods";
 
 export default function FeatureInfo() {
+    const [income, setIncome] = useState([]);
+    const [perc, setPerc] = useState(0);
+  
+    useEffect(() => {
+      const getIncome = async () => {
+        try {
+          const res = await userRequest.get("orders/income");
+          setIncome(res.data);
+          setPerc((res.data[1].total * 100) / res.data[0].total - 100);
+        } catch {}
+      };
+      getIncome();
+    }, []);
   return (
     <div className="featured row">
         <div className="featuredItem col-sm-12 col-md-3">
             <span className='featuredTitle'>Revanue</span>
             <div className="featuredMoneyContainer">
-                <span className='faturedMoney'>$2,415</span>
-                <span className='faturedMoneyRate'>+1,000 <ArrowUpwardIcon className="featuredIcon" /></span>
+                <span className='faturedMoney'>${income[1]?.total}</span>
+                <span className='faturedMoneyRate'>%{Math.floor(perc)}{" "}
+            {perc < 0 ? (<ArrowDownwardIcon className="featuredIcon negative"/>):( <ArrowUpwardIcon className="featuredIcon" />)}</span>
             </div>
             <spam className="featuredSub">Compared to last mounth</spam>
         </div>
